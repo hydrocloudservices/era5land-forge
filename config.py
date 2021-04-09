4f3a14f26069d98c78c7f702c7832bfda7d84e73
Sebastien Langlois <42305216+sebastienlanglois@users.noreply.github.com>
2021-04-09 17:34:35 -0400

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
 
     # Dataset
     START_DATE = "2020-01-01"
-    END_DATE = (datetime.utcnow() - timedelta(days=120)).strftime('%Y-%m-%d')
+    END_DATE = (datetime.utcnow() - timedelta(days=102)).strftime('%Y-%m-%d')
 
     VARIABLES = {
         '2m_dewpoint_temperature': 'd2m',

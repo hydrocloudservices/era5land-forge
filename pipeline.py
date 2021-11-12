5265348812dd2b127d25c3726162484a9e219c1b
Sebastien Langlois <42305216+sebastienlanglois@users.noreply.github.com>
2021-11-12 11:00:20 -0500

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -12,7 +12,7 @@ from itertools import product
 from config import Config
 
 
-def fetch_era5(date, variables_long_name):
+def fetch_era5(date, variables_long_name, download_filename):
     c = cdsapi.Client()
 
     name = 'reanalysis-era5-land'
@@ -28,7 +28,7 @@ def fetch_era5(date, variables_long_name):
 
     r = c.retrieve(name,
                    request,
-                   'tmp.nc')
+                   download_filename)
 
 
 @task()
@@ -70,26 +70,32 @@ def save_unique_variable_date_file(dates_vars):
     variables_long_name: list = [var_long_name
                                  for var_long_name, var_short_name in Config.VARIABLES.items()
                                  if var_short_name in list(map(lambda x: x.lower(), variables))]
+        
+    download_filename = "tmp-{:04d}{:02d}{:02d}".format(chosen_date.year,
+                                                        chosen_date.month,
+                                                        chosen_date.day)
 
-    fetch_era5(chosen_date, variables_long_name)
+    fetch_era5(chosen_date, variables_long_name, download_filename)
 
-    ds = xr.open_mfdataset('tmp.nc')
-    if 'expver' in list(ds.dims):
-        ds = ds.reduce(np.nansum, 'expver')
+    ds = xr.open_dataset(download_filename)
+    if all(pd.date_range(chosen_date, periods=24, freq='H') == ds.time.values):
+        if 'expver' in list(ds.dims):
+            ds = ds.reduce(np.nansum, 'expver')
 
-    for var in list(variables):
-        filename = "{:04d}{:02d}{:02d}_{}_ERA5_LAND_REANALYSIS.nc".format(chosen_date.year,
-                                                                        chosen_date.month,
-                                                                        chosen_date.day,
-                                                                        var.upper())
+        for var in list(variables):
+            filename = "{:04d}{:02d}{:02d}_{}_ERA5_LAND_REANALYSIS.nc".format(chosen_date.year,
+                                                                              chosen_date.month,
+                                                                              chosen_date.day,
+                                                                              var.upper())
 
-        ds[var.lower()].to_netcdf(filename)
-        print(filename)
-        fs.put(filename,
-               os.path.join(Config.BUCKET,
-                            filename))
-        os.remove(filename)
-    os.remove('tmp.nc')
+            ds[var.lower()].to_netcdf(filename)
+            print(filename)
+            print(ds[var.lower()])
+            fs.put(filename,
+                   os.path.join(Config.BUCKET,
+                                filename))
+            os.remove(filename)
+    os.remove(download_filename)
 
 
 if __name__ == '__main__':

7e6ed582173ed9e3bde2382eb77c5e4149fcfab1
Sebastien Langlois <42305216+sebastienlanglois@users.noreply.github.com>
2022-06-13 12:22:11 -0400

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -9,15 +9,16 @@ import pandas as pd
 import itertools
 import numpy as np
 from itertools import product
+import glob
 from config import Config
 
 
-def fetch_era5(date, variables_long_name, download_filename):
+def fetch_era5(date, variables_long_name):
     c = cdsapi.Client()
 
     name = 'reanalysis-era5-land'
 
-    request = {'format': 'netcdf',
+    request = {'format': 'grib',
                'variable': variables_long_name,
                'area': [63, -96, 40, -52],  # North, West, South, East. Default: global,
                'year': "{:04d}".format(date.year),
@@ -28,11 +29,11 @@ def fetch_era5(date, variables_long_name, download_filename):
 
     r = c.retrieve(name,
                    request,
-                   download_filename)
-
+                   'tmp.grib2')
 
 @task()
 def list_available_data_not_in_bucket():
+
     """
     Determines list of all possible unique single variable daily files from a list of dates.
     It then compares if those files exist in the bucket (Config.BUCKET)
@@ -65,40 +66,65 @@ def save_unique_variable_date_file(dates_vars):
 
     fs = fsspec.filesystem('s3', **Config.STORAGE_OPTIONS)
 
-    chosen_date, variables = dates_vars
-    chosen_date = datetime.strptime(chosen_date, '%Y%m%d')
+    chosen_date_str, variables = dates_vars
+
+    chosen_date = datetime.strptime(chosen_date_str, '%Y%m%d')
     variables_long_name: list = [var_long_name
                                  for var_long_name, var_short_name in Config.VARIABLES.items()
                                  if var_short_name in list(map(lambda x: x.lower(), variables))]
-        
-    download_filename = "tmp-{:04d}{:02d}{:02d}".format(chosen_date.year,
-                                                        chosen_date.month,
-                                                        chosen_date.day)
+    print(chosen_date)
+    # # add case when return no data
+    fetch_era5(chosen_date, variables_long_name)
 
-    fetch_era5(chosen_date, variables_long_name, download_filename)
+    import cfgrib
 
-    ds = xr.open_dataset(download_filename)
-    if all(pd.date_range(chosen_date, periods=24, freq='H') == ds.time.values):
+    ds_list = cfgrib.open_datasets('tmp.grib2')
+
+    for ds in ds_list:
+        print(ds.valid_time)
+        # ds = ds.squeeze()
+
+        if 'step' in list(ds.dims):
+            if any(ds.time.shape) == True:
+                ds = ds.stack(z=('time', 'step')).reset_index('z')
+                # ds['z'] = ds.time + ds.step.fillna(0)
+                ds['z'] = (ds.time + ds.step.fillna(0)).values.reshape(-1)
+                cleaned_list = [x for x in list(ds.coords.keys()) if x not in ['latitude', 'longitude', 'z']]
+                ds = ds.drop(cleaned_list) \
+                    .dropna('z', how='all') \
+                    .rename({'z': 'time'})
+            else:
+                ds['step'] = (ds.time + ds.step.fillna(0)).values.reshape(-1)
+                cleaned_list = [x for x in list(ds.coords.keys()) if x not in ['latitude', 'longitude', 'step']]
+                ds = ds.drop(cleaned_list) \
+                    .dropna('step', how='all') \
+                    .rename({'step': 'time'})
+        else:
+            cleaned_list = [x for x in list(ds.coords.keys()) if x not in ['latitude', 'longitude', 'time']]
+            ds = ds \
+                .drop(cleaned_list)
+
         if 'expver' in list(ds.dims):
             ds = ds.reduce(np.nansum, 'expver')
 
-        for var in list(variables):
+        for var in list(ds.keys()):
             filename = "{:04d}{:02d}{:02d}_{}_ERA5_LAND_REANALYSIS.nc".format(chosen_date.year,
                                                                               chosen_date.month,
                                                                               chosen_date.day,
                                                                               var.upper())
-
             ds[var.lower()].to_netcdf(filename)
             print(filename)
-            print(ds[var.lower()])
             fs.put(filename,
                    os.path.join(Config.BUCKET,
                                 filename))
             os.remove(filename)
-    os.remove(download_filename)
+    os.remove('tmp.grib2')
+    for f in glob.glob("tmp*.idx"):
+        os.remove(f)
 
 
 if __name__ == '__main__':
+
     with Flow("ERA5-ETL") as flow:
         dates_vars: np.array = list_available_data_not_in_bucket()
         save_unique_variable_date_file.map(dates_vars)
